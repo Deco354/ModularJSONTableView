882ba49fb168c1b966f51d61b91a1533c0cb6080
Declan McKenna <deco354@gmail.com>
2020-06-22 13:08:18 +0100

    Initial commit

---
+++ ModularJSONTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ModularJSONTableView
+//
+//  Created by Declan McKenna on 22/06/2020.
+//  Copyright © 2020 Declan McKenna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
