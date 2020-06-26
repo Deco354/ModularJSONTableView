077af949523bed0b0189b8c2c34d90715d31f6fb
Declan McKenna <deco354@gmail.com>
2020-06-26 15:57:16 +0100

    Create Card model
    This includes a CardResource that complies with APIResource for the APIRequest to use

---
+++ ModularJSONTableView/Card.swift
@@ -0,0 +1,33 @@
+//
+//  Card.swift
+//  ModularJSONTableView
+//
+//  Created by Declan McKenna on 26/06/2020.
+//  Copyright © 2020 Declan McKenna. All rights reserved.
+//
+
+import Foundation
+
+import Foundation
+import UIKit.UIImage
+
+struct CardResource: APIResource {
+    typealias ModelType = Card
+    var url = URL(string: "https://deckofcardsapi.com/api/deck/new/draw/?count=52")!
+}
+
+struct Cards: Decodable {
+    let cards: [Card]
+}
+
+struct Card: Decodable {
+    let imageURL: URL
+    let value: String
+    let suit: String
+    var image: UIImage? // This should be adapted to have value semantics
+    
+    private enum CodingKeys: String, CodingKey {
+        case imageURL = "image"
+        case value, suit
+    }
+}
